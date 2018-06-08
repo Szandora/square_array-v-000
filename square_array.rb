f53ad09c68a04682f42085c43a880cba39d415b4
Sheila Wittman <szandora789@gmail.com>
2018-06-08 20:21:01 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   square_array = []
   array.each do |number|
-    x = number**2
-    square_array << number
+    square_array << number ** 2
   end
+  return square_array
 end
